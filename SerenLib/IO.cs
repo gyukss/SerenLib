354aed70418759c86e5bb952894469fe6064c14c
gyukss <serensaint9@gmail.com>
2026-02-02 19:06:50 +0000

    Add project files.

---
+++ SerenLib/IO.cs
@@ -0,0 +1,24 @@
+﻿namespace SerenLib.IO
+{
+   
+    public static class IO
+    {
+        /// <summary>
+        /// Animates typewriter effect
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="delay">Delay in ms between each letter</param>
+        public static void Out(string message, int delay = 50)
+        {
+            // Each character in the string
+            foreach(char i in message)
+            {
+                // Output and wait before next character
+                Console.Write(i);
+                Thread.Sleep(delay);
+            }
+            // Create new line once done
+            Console.WriteLine();
+        }
+    }
+}
